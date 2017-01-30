22556cc389b88d104ee6fae03c45b9f0afefcf35
Vedran Blazenka <vedran.blazenka@gmail.com>
2017-01-30 19:30:31 +0100

    Added basic questions for module creation

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,7 +1,9 @@
 'use strict';
-var Generator = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+const Generator = require('yeoman-generator');
+const normalizeUrl = require('normalize-url');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const humanizeUrl = require('humanize-url');
 const _s = require('underscore.string');
 
 module.exports = Generator.extend({
@@ -11,16 +13,38 @@ module.exports = Generator.extend({
       'Welcome to the ' + chalk.red('generator-nome') + ' generator!'
     ));
 
-    var prompts = [{
-      name: ' moduleName',
-      message: 'What do you want to name your module?',
-			default: _s.slugify(this.appname),
-      filter: x => moduleName.slugify(x)
-    }];
+    const prompts = [
+      {
+        name: 'moduleName',
+        message: 'What do you want to name your module?',
+        default: _s.slugify(this.appname),
+        filter: x => moduleName.slugify(x)
+      },
+      {
+        name: 'moduleDescription',
+        message: 'What is your module description?',
+        default: `My ${superb()} module`
+      },
+      {
+        name: 'githubUsername',
+        message: 'What is your GitHub username?',
+        store: true,
+        validate: x => x.length > 0 ? true : 'You have to provide a username',
+      },
+      {
+        name: 'website',
+        message: 'What is the URL of your website?',
+        store: true,
+        validate: x => x.length > 0 ? true : 'You have to provide a website URL',
+        filter: x => normalizeUrl(x)
+      }
+    ];
 
     return this.prompt(prompts).then(function (props) {
       // To access props later use this.props.someAnswer;
       this.props = props;
+
+      console.log('This props')
     }.bind(this));
   },
 
